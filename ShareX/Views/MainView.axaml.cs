e6105649bb403dde4dfc95f971b1f5a94764e107
Z9TONEDEVELOPER <maxalex1018@mail.ru>
2026-04-07 23:03:10 +0300

    init

---
+++ ShareX/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ShareX.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ShareX/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ShareX.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
