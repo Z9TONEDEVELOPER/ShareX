ad63a5fdb01bbacf1c5c2279f84b51c9f4c6a2c6
Z9TONEDEVELOPER <maxalex1018@mail.ru>
2026-04-11 02:04:02 +0300

    send rececive tab

---
+++ ShareX/Views/ReceiveView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ShareX.Views;
+
+public partial class ReceiveView : UserControl
+{
+    public ReceiveView()
+    {
+        InitializeComponent();
+    }
+}
